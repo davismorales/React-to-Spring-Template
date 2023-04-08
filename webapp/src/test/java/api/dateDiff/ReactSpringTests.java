aef9fb4d73fb2ef4a01461d5359c8f03c936683b
davismorales <DAVISMORALES12@GMAIL.COM>
2023-04-08 15:09:59 -0500

    Testing package edits

--- webapp/src/test/java/api/dateDiff/ReactSpringTests.java
+++ webapp/src/test/java/api/dateDiff/ReactSpringTests.java
@@ -1,10 +1,10 @@
-package com.DateDiff;
+package api.dateDiff;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReactSpringTests {
+class DateDiffTests {
 
 	@Test
 	void contextLoads() {
